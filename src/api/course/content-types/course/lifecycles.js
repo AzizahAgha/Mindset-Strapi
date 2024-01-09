52c3208c4a7d108de5aa2a12e0519af8b9564fe5
sonal.nanodkar <sonal.nanodkar@kilowott.com>
2024-01-09 21:18:30 +0530

    Added call to get download links

--- src/api/course/content-types/course/lifecycles.js
+++ src/api/course/content-types/course/lifecycles.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
-var  vidData="";
+var  vidData=[];
+var token="";
 
 module.exports = {
 
@@ -25,8 +26,8 @@ module.exports = {
         }
        
       ).then(async function(response){
-        
-      await  axios.get(event.params.data.showcase_vimeo_url //"https://api.vimeo.com/users/213493461/albums/10888770/videos" 
+        token = await response.data.access_token;
+        await axios.get(event.params.data.showcase_vimeo_url //"https://api.vimeo.com/users/213493461/albums/10888770/videos" 
         ,
         {
           headers: {
@@ -41,6 +42,29 @@ module.exports = {
           console.log("errrrorrr",error.message)
        })
     });
+    console.log(vidData.length);
+    
+    for(let i=0;i<vidData.length;i++) 
+    {
+      
+      console.log("i",i);
+     var downloadUri = `https://api.vimeo.com${vidData[i].uri}`;
+     var downloadLink = "";
+       axios.get(downloadUri
+        ,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      ).then(async function(res){
+         downloadLink = await res.data.data;
+      console.log("res.data",res);
+        })
+
+        vidData[i].download_link = "\""+ downloadLink + "\"";
+        console.log("vidData", vidData[i]);
+    }
     event.params.data.video = vidData;
 },
 
